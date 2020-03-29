40f706b90183f3ae655fd18179a649926abad15c
João Henrique <joaohenrique.hs@hotmail.com>
2020-03-29 16:44:55 -0300

    Removed unused Template and View directories

--- index.php
+++ index.php
@@ -12,20 +12,12 @@ require_once 'App/Models/Comment.php';
 
 require_once 'lib/Database/Connection.php';
 
-require_once 'vendor/autoload.php';
-
 //-------------------------------------------------
-
-$template = file_get_contents("App/Template/estrutura.html");
 
 ob_start();
 $core = new Core;
 $core->start();
 $retorno = ob_get_contents();
 ob_end_clean();
-
-// $tplPronto = str_replace('{{conteudo}}', $retorno, $template);
-
-// echo $tplPronto;
 
 echo $retorno;
